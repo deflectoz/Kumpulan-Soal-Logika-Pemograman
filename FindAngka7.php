597664cc6cf48c69db2902106ab00b9fb4229097
Muhammad Febriyan Akbar <muh.febrian@gmail.com>
2022-04-19 22:15:09 +0700

    Create FindAngka7.php
    Mencari total angka 7 dalam bilangan 1 - 100

---
+++ FindAngka7.php
@@ -0,0 +1,18 @@
+<?php
+
+$output = 0;
+
+for($i=1; $i<=100; $i++){
+	$slpInput = str_split($i);
+  for($a=0;$a<count($slpInput); $a++){
+  	if($slpInput[$a] == 7)
+    {
+    	$output++;
+      echo $i ."\n" ;
+      }
+  }
+}
+// echo $output;
+// echo 'Total Angka 7 dari bilangan 1 - 100 adalah '.$output;
+
+?>
